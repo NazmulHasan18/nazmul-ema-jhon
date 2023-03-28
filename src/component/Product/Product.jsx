8a749a10f92aa102faefffb257a5b5c5e7819405
NazmulHasan18 <nazmul182218@gmail.com>
2023-03-28 14:40:28 +0600

    card compo fixed

--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -7,12 +7,14 @@ const Product = (props) => {
    return (
       <div className="product">
          <img src={img} alt="" />
-         <h6 className="name">{name}</h6>
-         <h5 className="price">Price: ${price}</h5>
+         <div className="title-container">
+            <h6 className="name">{name}</h6>
+            <h5 className="price">Price: ${price}</h5>
+         </div>
          <p className="seller">Manufacture: {seller}</p>
-         <p className="ratings">Ratings: {ratings}</p>
-         <hr />
-         <button>Add To Cart</button>
+         <p className="ratings">Ratings: {ratings} star</p>
+
+         <button className="btn-cart">Add To Cart</button>
       </div>
    );
 };
